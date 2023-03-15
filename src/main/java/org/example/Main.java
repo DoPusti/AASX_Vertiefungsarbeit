496caf463b2ba0f6958e8fce179bf6b31cb469cc
Dominik Pustofka <dominik.pustofka@gmx.net>
2023-03-15 22:37:13 +0100

    Anzahl von trues in einem array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+    public static int calculator(int num1, char operator, int num2) {
+        switch (operator) {
+            case '*' : return num1 * num2;
+            case '/' : if(num2==0) {return 0 ; }else {return num1 / num2;}
+            case '+' : return num1 + num2;
+            case '-' : return num1 - num2;
+
+        }
+        return 0;
+
+    }
+    // Anzahl von trues in einem Array
+    public static int countTrue(boolean[] arr) {
+        Stream<Boolean> stream = Booleans.asList(arr).stream();
+        return (int)Arrays.stream(arr)
+                .filter(c -> c!=null && c)
+                .count();
     }
 }
